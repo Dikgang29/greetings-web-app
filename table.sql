ac30f0afe4582734101f95fa09238c4ea5e86181
Dikgang29 <dcmokonyama29@gmail.com>
2022-10-12 01:53:06 +0200

    make it better

--- table.sql
+++ table.sql
@@ -1,9 +1,9 @@
-create table user_names(
-	id serial not null primary key,
-	users_greeted VARCHAR ( 50 ) UNIQUE NOT NULL,
-	name_counter int,
-	description text not null
-);
+-- create table user_names(
+-- 	id serial not null primary key,
+-- 	users_greeted VARCHAR ( 50 ) UNIQUE NOT NULL,
+-- 	name_counter int,
+-- 	description text not null
+-- );
 create table user_names(
 	id serial not null primary key,
 	users_greeted VARCHAR ( 50 ) UNIQUE NOT NULL,
