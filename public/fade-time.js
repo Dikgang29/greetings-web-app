653d9a61843292f89232d4561153b1656c861f16
Dikgang29 <dcmokonyama29@gmail.com>
2022-10-12 03:02:22 +0200

    fade time

---
+++ public/fade-time.js
@@ -0,0 +1,5 @@
+setTimeout(function(){
+    document.querySelector(".testError").innerHTML = '';
+    document.querySelector(".testFlash").innerHTML = '';
+    document.querySelector(".greetMsg").innerHTML = '';
+}, 4000);
