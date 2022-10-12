2c0ebfc4978b7fdbfdb7b49d3995ccf9fafc102c
Dikgang29 <dcmokonyama29@gmail.com>
2022-10-12 23:13:06 +0200

    database tests

--- test/greetweb.test.js
+++ test/greetweb.test.js
@@ -1,30 +1,30 @@
-let assert = require('assert');
-let greeting = require('../greetweb');
+// let assert = require('assert');
+// let greeting = require('../greetweb');
 
-describe('the Greetings Function - when enlish is checked', ()=>{
-    it('should be able to greet Dikgang in English',()=>{
-        const greet = greeting();
-        greet.setGreeting('english', 'Dikgang');
-        assert.equal('Hello, Dikgang', greet.getGreetMessage().message, greet.getGreetMessage().personName);
-    });
-describe('the Greetings Function - when sepedi is checked', ()=>{
+// describe('the Greetings Function - when enlish is checked', ()=>{
+//     it('should be able to greet Dikgang in English',()=>{
+//         const greet = greeting();
+//         greet.setGreeting('english', 'Dikgang');
+//         assert.equal('Hello, Dikgang', greet.getGreetMessage().message, greet.getGreetMessage().personName);
+//     });
+// describe('the Greetings Function - when sepedi is checked', ()=>{
 
-    it('should be able to greet Dikgang in Sepedi',()=>{
-        const greet = greeting();
-        greet.setGreeting('sepedi', 'Dikgang');
-        assert.equal('Dumela, Dikgang', greet.getGreetMessage().message, greet.getGreetMessage().personName);
-    });
-});
+//     it('should be able to greet Dikgang in Sepedi',()=>{
+//         const greet = greeting();
+//         greet.setGreeting('sepedi', 'Dikgang');
+//         assert.equal('Dumela, Dikgang', greet.getGreetMessage().message, greet.getGreetMessage().personName);
+//     });
+// });
 
-describe('the Greetings Function - when Tsonga is checked', ()=>{
+// describe('the Greetings Function - when Tsonga is checked', ()=>{
 
-    it('should be able to greet Dikgang in Tsonga',()=>{
-        const greet = greeting();
-        greet.setGreeting('tsonga', 'Dikgang');
-        assert.equal('Minjhani, Dikgang', greet.getGreetMessage().message, greet.getGreetMessage().personName);
-    });
-});
+//     it('should be able to greet Dikgang in Tsonga',()=>{
+//         const greet = greeting();
+//         greet.setGreeting('tsonga', 'Dikgang');
+//         assert.equal('Minjhani, Dikgang', greet.getGreetMessage().message, greet.getGreetMessage().personName);
+//     });
+// });
 
 
 
-})
+// })
